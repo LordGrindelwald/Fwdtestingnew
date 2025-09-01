ad32d5d45cc0a791dbe3579293456997b732d3f5
Ian Grindelwald <164880317+LordGrindelwald@users.noreply.github.com>
2025-09-01 10:38:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from bot import Bot
+
+# Create an instance of the bot
+app = Bot()
+
+if __name__ == "__main__":
+    # Run the bot (this will also start the user client as defined in bot.py)
+    app.run()
